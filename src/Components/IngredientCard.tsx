e76ddc9ca32d3f988e1bf2bba814083f81d86b5a
CoutCout <arthur.couturier.2000@gmail.com>
2024-03-08 19:00:24 +0100

    connexion à l'api rest faite en spring boot

---
+++ src/Components/IngredientCard.tsx
@@ -0,0 +1,37 @@
+import Button from "./Button.tsx";
+import React from "react";
+
+function IngredientCard({children, ingredientJson, className, clickIngredientHandler}:
+                            {
+                                children?: React.ReactNode,
+                                ingredientJson: object,
+                                className?: string,
+                                clickIngredientHandler: (name: string) => Promise<void>
+                            }) {
+
+    const ingredientUuid: string = ingredientJson.uuid;
+    const ingredientName: string = ingredientJson.name;
+    const ingredientDesc: string = ingredientJson.desc;
+
+    const handler = async () => {
+        await clickIngredientHandler(ingredientUuid);
+    }
+
+    return (
+        <>
+            <Button className={`p-2 mx-3 my-3 border-2 rounded-xl flex flex-col ${className}`} onClick={handler}>
+                <div>
+                    {ingredientName}
+                </div>
+                <div>
+                    {ingredientDesc}
+                </div>
+                <div>
+                    {children}
+                </div>
+            </Button>
+        </>
+    )
+}
+
+export default IngredientCard;
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,18 +1,70 @@
-import Card from "../Components/Card.tsx";
+import IngredientCard from "../Components/IngredientCard.tsx";
 import Button from "../Components/Button.tsx";
+import axios from "axios";
+import {useEffect, useState} from "react";
+
+async function getAllIngredients() {
+    const response = await axios.get('http://127.0.0.1:8080/api/ingredients/getAll');
+    return response.data;
+}
+
+async function createIngredient(name: string, desc: string) {
+    const data = {"name": name, "desc": desc};
+    const response = await axios.post('http://127.0.0.1:8080/api/ingredients/create', data);
+    return response.data;
+}
+
+async function deleteIngredient(uuid: string) {
+    const response = await axios.delete('http://127.0.0.1:8080/api/ingredients/deleteByUUid/' + uuid);
+    return response.data;
+}
 
 function Home() {
+
+    const [ingredients, setIngredients] = useState([]);
+    const [nameInput, setNameInput] = useState("");
+    const [descInput, setDescInput] = useState("");
+
+    const fetchIngredients = async () => {
+        const ingredientsData = await getAllIngredients();
+        setIngredients(ingredientsData);
+    };
+
+    useEffect(() => {
+        fetchIngredients();
+    }, []);
+
+    const clickAddIngredientHandler = async () => {
+        if (nameInput != "" && descInput != "") {
+            await createIngredient(nameInput, descInput);
+            fetchIngredients();
+            setNameInput("");
+            setDescInput("");
+        }
+    }
+
+    const clickIngredientHandler = async (uuid: string) => {
+        await deleteIngredient(uuid);
+        fetchIngredients();
+    }
+
     return (
         <>
-            <div className={"flex items-center justify-center flex-grow"}>
+            <div className={"flex items-center justify-center flex-col"}>
                 <div className={"flex"}>
                     Home Page
                 </div>
                 <div className={"flex items-center justify-center"}>
-                    <Card />
+                    {ingredients.map(ingredient => (
+                        <IngredientCard ingredientJson={ingredient} clickIngredientHandler={clickIngredientHandler} />
+                    ))}
                 </div>
-                <Button>
-                    Button
+                <input value={nameInput} onChange={(e) => setNameInput(e.target.value)}
+                        className={"my-2"}/>
+                <input value={descInput} onChange={(e) => setDescInput(e.target.value)}
+                       className={"my-2"}/>
+                <Button onClick={clickAddIngredientHandler}>
+                    Ajouter un ingrédient
                 </Button>
             </div>
         </>
