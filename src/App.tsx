96ea6f8cb13066a6ad27288096e1827b89964a5e
CoutCout <72403468+ArthurCouturier@users.noreply.github.com>
2024-04-20 09:58:08 +0200

    deleted all peppers view and changed header as a consequence

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 import Footer from "./Components/Footer.tsx";
 import Header from "./Components/Header.tsx";
 import About from "./Pages/About.tsx";
-import AllPeppers from "./Pages/AllPeppers.tsx";
 import Home from "./Pages/Home.tsx";
 import PepperPage from "./Pages/PepperPage.tsx";
 import PeppersTypePage from "./Pages/PeppersTypePage.tsx";
@@ -15,7 +14,7 @@ function App() {
     <BrowserRouter>
       <Header className={" bg-white font-josefin"} />
       <div
-        className={"bg-force flex-grow h-[87vh] overflow-y-scroll font-cinzel"}
+        className={"bg-force flex-grow h-[87vh] overflow-y-scroll font-cinzel text-white"}
         id={"root"}
       >
         <Routes>
@@ -23,10 +22,10 @@ function App() {
           < Route path="/" element={< Home />} />
           < Route path="/about" element={< About />} />
           < Route path="/profile" element={< ProfilePage />} />
-          < Route path="/pepper" element={< AllPeppers />} />
-          < Route path="/pepper/:id" element={< PepperPage />} />
-          < Route path="/specification" element={< SpecificationPage />} />
-          < Route path="/pepper/type/:type" element={< PeppersTypePage />} />
+          {/* < Route path="/pepper" element={< AllPeppers />} /> */}
+          < Route path="/pepper/:pepperUuid" element={< PepperPage />} />
+          < Route path="/specification/:specification" element={< SpecificationPage />} />
+          < Route path="/peppers/:pepperType" element={< PeppersTypePage />} />
         </Routes>
       </div>
       <Footer className={"bg-opacity-10 bg-white mt-auto"} />
